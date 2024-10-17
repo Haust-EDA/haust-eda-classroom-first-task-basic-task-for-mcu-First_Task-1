7e3497efe62028fc7eebce4f63f9d680aeced65a
jw0220st <3325971870@qq.com>
2024-10-17 16:49:24 +0800

    Update task.c

--- task.c
+++ task.c
@@ -1,12 +1,12 @@
 
 
-void delay_ms   ( unsigned int x )     // 延迟函数
+void delay_ms     ( unsigned int x )         // 延迟函数
 {
     无符号整型i,j;
     如果（x== 1000）
     {
-        for     ( i= 0 ;i< 19601 ;i++ )      // 延迟 1s
-        {
+        for           ( i= 0 ;i< 19601 ;i++ )              // 延迟 1s
+		{
             对于( j= 5 ;j> 0 ;j-- ) ;
         }
     }
@@ -18,30 +18,30 @@ void delay_ms   ( unsigned int x )     // 延迟函数
 
 int 主函数（无效）
 {
-   // ”“”在下面编写了你的​​​​​​代码
+   // ”“”在下面编写了你的​​​​​​​​​​​​​​​​​​​​代码
     
-    RCC_APB2PeriphClockCmd（ RCC_APB2Periph_GPIOA，ENABLE）；
+    RCC_APB2PeriphClockCmd（ RCC_APB2Periph_GPIOA，启用）；
     GPIO_InitTypDef A;
     A、GPIO_mode =GPIO_Mode_Out_PP；	  
 	A、GPIO_Pin = GPIO_Pin_All；				
 	A、GPIO_speed = GPIO_speed_50MHz；
-    while(1)
+	while(1)
 	{
         
-                GPIO_Write(GPIOA, ~0x0001);
-		Delay_ms(100);				
-		GPIO_Write(GPIOA, ~0x0002);	
-		Delay_ms(100);	
-                GPIO_Write(GPIOA, ~0x0004);
-		Delay_ms(100);				
-		GPIO_Write(GPIOA, ~0x0008);
-		Delay_ms(100);				
-		GPIO_Write(GPIOA, ~0x0010);	
-		Delay_ms(100);				
-		GPIO_Write(GPIOA, ~0x0020);	
-		Delay_ms(100);				
-		GPIO_Write(GPIOA, ~0x0040);	
-		Delay_ms(100);				
+                GPIO_Write （ GPIOA，〜0x0001）；
+		延迟_毫秒（100）；				
+		GPIO_Write （ GPIOA，〜0x0002）；	
+		延迟_毫秒（100）；	
+                GPIO_Write （ GPIOA，〜0x0004）；
+		延迟_毫秒（100）；				
+		GPIO_Write （ GPIOA，〜0x0008）；
+		延迟_毫秒（100）；				
+		GPIO_Write （ GPIOA，〜0x0010）；	
+		延迟_毫秒（100）；				
+		GPIO_Write （ GPIOA，〜0x0020）；	
+		延迟_毫秒（100）；				
+		GPIO_Write （ GPIOA，〜0x0040）；	
+		延迟_毫秒（100）；				
     }
 }
   
